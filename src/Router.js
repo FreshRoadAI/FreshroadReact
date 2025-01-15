73bfc8e9ad992d7249d250f0a9f1aa76346dfdc4
ALLEN\allen <allenchak95@gmail.com>
2025-01-14 16:29:21 -0800

    Big Changes

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,8 @@ import App from './pages/Main'; // Your main page
 import Courses from './pages/Courses'; // Import the pages
 import Resources from './pages/Resources';
 import CivicsLearningPage from './pages/Civicslearning';
+import CivicsExamPage from './pages/CivicsExam';
+import ChatInterfacePage from './pages/Chatpage';
 
 const AppRouter = () => {
   return (
@@ -13,6 +15,8 @@ const AppRouter = () => {
         <Route path="/courses" element={<Courses />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/courses/civics-101" element={<CivicsLearningPage />} />
+        <Route path="/tests/us-civics" element={<CivicsExamPage />} />
+        <Route path="/resources/211-call-center" element={<ChatInterfacePage />} />
       </Routes>
     </Router>
   );
